18378e78202f7f7293a4320d9bf0fcd2d4e99617
Alberto <alberto.alvarez.sanz@gmail.com>
2021-03-01 21:50:31 +0100

    Initial commit

---
+++ src/apuntes.kt
@@ -0,0 +1,21 @@
+
+/*
+
+VER LISTA
+curl -v localhost:8081/students
+
+VER UNO
+curl -v localhost:8081/student/1
+
+INSERTAR
+curl --request POST  --header "Content-type:application/json" --data "{\"id\":99, \"name\":\"Estudiante\", \"surname\":\"de Prueba\"}" localhost:8081/student
+
+BORRAR
+curl -X DELETE localhost:8081/student/1
+
+
+
+https://github.com/AlbertoAlvarezSanzW/connex
+https://github.com/carlosbellmont/IntroduccionKotlinCoroutines
+
+ */
